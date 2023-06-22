4aaaa627a979db3679a80c36f73d396c748f330e
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2023-06-22 15:05:09 +0300

    feat: add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
 class User < ApplicationRecord
+  validates :first_name, :last_name, :username, :email, presence: true
+  validates :username, :phone, :email, uniqueness: true
 end
