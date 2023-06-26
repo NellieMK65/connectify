de9139a769e437aaa8bf55039ba5a1df51e6ee9e
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2023-06-26 12:24:21 +0300

    feat: add cookies & sessions middleware

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+class SessionsController < ApplicationController
+  def index
+    session[:session_hello] ||= "World"
+    cookies[:cookies_hello] ||= "World"
+
+    render json: { session: session, cookies: cookies.to_hash }
+  end
+end
